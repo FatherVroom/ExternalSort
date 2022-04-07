f5ad7c2e078ec5d196f20f20d842bc2aa4ef77ad
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2022-04-06 22:16:58 -0400

    Add files via upload

---
+++ InputBuffer.java
@@ -0,0 +1,74 @@
+import java.nio.ByteBuffer;
+
+/**
+ * InputBuffer class which takes in a byte array
+ * and is meant to convert this byte array into an array of Records of size 512.
+ * The input buffer only has space for 512 records, or 1 block of records. These
+ * records can be replaced by other blocks.
+ * 
+ * @author Aniket Adhikari
+ * @version 6 April 2022
+ */
+public class InputBuffer {
+    private Record[] records;
+    private static final int BUFFER_CAPACITY = 8192;
+    private static final int NUM_RECORDS = 512;
+    private byte[] block;
+    private int size;
+
+    /**
+     * Constructor for an InputBuffer
+     * 
+     * @param block
+     *            is what is being converted into an array of records
+     */
+    public InputBuffer(byte[] block) {
+        this.block = block;
+        size = NUM_RECORDS;
+        records = new Record[NUM_RECORDS];
+    }
+
+
+    /**
+     * Fills the InputBuffer with an array of Records.
+     */
+    public void fillRecords() {
+        // make bytebuffer out of the block of code that is passed in
+        ByteBuffer b = ByteBuffer.wrap(block);
+        int recCounter = 0;
+        // cycle through the block of 8,192 bytes and convert to record objects
+        for (int i = 0; i < BUFFER_CAPACITY; i += 16) {
+            byte[] newRecord = new byte[16]; // create byte array to store a
+                                             // record
+            b.get(newRecord, i, 16); // store block contents into the byte array
+                                     // newRecord
+            Record r = new Record(newRecord); // store the byte array into an
+                                              // actual record
+            records[recCounter] = r; // place the record in the record array
+            recCounter++;
+        }
+    }
+
+
+    /**
+     * Gets the Records in an array format
+     * 
+     * @return records inside the Buffer
+     */
+    public Record[] getRecords() {
+        // make a precondition to ensure record field has been populated
+        return records;
+    }
+
+
+    /**
+     * Determines whether there is space in the Buffer for blocks of records to
+     * be placed inside off
+     * 
+     * @return true of the InputBuffer is empty
+     */
+    public boolean isEmpty() {
+        // use for when we want to start filling up input buffer again
+        return size == 0;
+    }
+}
---
+++ Parser.java
@@ -0,0 +1,81 @@
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+
+/**
+ * Able to go through the blocks of bytes (which can be
+ * thought of as an array of bytes) and read records inside the block. Each
+ * block contains 512 records and each record is 16 bytes of memory
+ * 
+ * @author Aniket Adhikari
+ * @version 2 April 2022
+ *
+ */
+public class Parser {
+    private static final int BLOCK_SIZE = 8192;
+    private static final int NUM_BLOCKS = 8;
+    private String fileName;
+    private RandomAccessFile raf;
+
+    /**
+     * Creates a Parser object, which is meant to go through fileName
+     * 
+     * @param fileName
+     *            the name of the file that is meant to be found
+     * @throws FileNotFoundException
+     *             if the file we want to parse doesn't exist in our folder
+     */
+    public Parser(String fileName) throws FileNotFoundException {
+        this.fileName = fileName;
+        try {
+            raf = new RandomAccessFile(fileName, "r");
+        }
+        catch (FileNotFoundException e) {
+            throw new FileNotFoundException("Could not find the file: "
+                + fileName);
+        }
+//        mhRecords = new MinHeap<Record>(null, 0, NUM_RECORDS * NUM_BLOCKS);
+    }
+
+
+    /**
+     * Takes the binary input file and constructs a byte array the size of a
+     * single block, which is 8192 bytes, and 512 "records" or key-value pairs
+     * 
+     * @return a block of data as a byte array of size 8192
+     * @throws IOException
+     *             when EOF has been reached
+     */
+    public byte[] getBlock() throws IOException {
+        // allocate space to create bytebuffer
+        ByteBuffer block = ByteBuffer.allocate(BLOCK_SIZE); 
+        try {
+            // fill byte buffer with contents of 1st block from fileName.bin
+            for (int i = 1; i <= BLOCK_SIZE; i++) {
+                byte b = raf.readByte();
+                block.put(b);
+            }
+        }
+        catch (EOFException e) {
+            throw new EOFException("End of the file titled, " + fileName
+                + ", has been reached");
+        }
+        return block.array();
+    }
+ 
+    /**
+     * 
+     * @return
+     * @throws IOException
+     */
+    public long numOfBlocks() throws IOException {
+        try {
+            return raf.length() / BLOCK_SIZE;
+        }
+        catch (IOException e) {
+            throw new IOException("EndOfFile");
+        }
+    }
+}
